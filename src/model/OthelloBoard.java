cb20be34dfb4fe3f80e1e69489d20332e46f779c
ada10lgu <ada10lgu@student.lth.se>
2014-01-30 22:43:22 +0100

    Model: Added some methods to the model

--- src/model/OthelloBoard.java
+++ src/model/OthelloBoard.java
@@ -2,6 +2,20 @@ package model;
 
 public class OthelloBoard {
 
+	public static enum Option{NONE,BLACK,WHITE};
 	
+	private Option[][] board;
+	
+	public OthelloBoard(int width, int height) {
+		board = new Option[width][height];
+	}
+	
+	public Option[][] getBoard() {
+		return board;
+	}
+	
+	public void setSquare(int x, int y, Option o) {
+		
+	}
 	
 }
