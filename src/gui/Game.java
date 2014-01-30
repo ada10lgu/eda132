3c5316e047fb7fc6aad3f0728ca1ec2b69928ecb
ada10lgu <ada10lgu@student.lth.se>
2014-01-31 00:59:10 +0100

    Game: Set start conditions
    Made so the game starts with the normal "cross" of 2 white and 2 black

--- src/gui/Game.java
+++ src/gui/Game.java
@@ -1,11 +1,17 @@
 package gui;
 
 import model.OthelloBoard;
+import static model.OthelloBoard.*;
 
 public class Game {
 	public static void main(String[] args) {
 		
 		OthelloBoard model = new OthelloBoard(8, 8);
+		model.setSquare(4, 4, WHITE);
+		model.setSquare(3, 3, WHITE);
+		model.setSquare(4, 3, BLACK);
+		model.setSquare(3, 4, BLACK);
+		
 		new Othello(model);
 		
 		
